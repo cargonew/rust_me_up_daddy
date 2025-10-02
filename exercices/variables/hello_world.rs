0064048b0a3cb038b5736ee574175b03724d2739
OKuhle <okuhlexesibe0@gmail.com>
2025-10-02 17:20:05 +0000

    First time .. might as well fall in love

---
+++ exercices/variables/hello_world.rs
@@ -0,0 +1,14 @@
+fn main(){
+    /**
+    * Use the let keyword to declare a new variable
+    * Followed by the variable name and this ":"a ( yes i do not know what this thing is called)
+    * and finally the type (int / string / or your own made data type(structs....etc))
+    * see example below
+    * */
+
+    let greet_me_daddy: String = "Hello World";
+
+    println!("{}", greet_me_daddy);  //This print is not a function, it is a macro(more on that later...)
+
+
+}
