aa5bf4d03a308cf2a1311b7d23ea35d59dc8aae0
OKuhle <okuhlexesibe0@gmail.com>
2025-10-05 20:50:25 +0000

    Hello this is rust

---
+++ src/exercices/conditionals/main.rs
@@ -0,0 +1,29 @@
+fn main(){ 
+    let x = 5;
+    let y = 10;
+
+    if x < y {
+        println!("x is less than y");
+    } else {
+        println!("x is greater than y");
+    }
+
+    if x > y {
+        println!("x is greater than y");
+    } else {
+        println!("x is less than y");
+    }
+
+    if x == y {
+        println!("x is equal to y");
+    } else {
+        println!("x is not equal to y");
+    }
+
+    if x != y {
+        println!("x is not equal to y");
+    } else {
+        println!("x is equal to y");
+    }
+
+}
